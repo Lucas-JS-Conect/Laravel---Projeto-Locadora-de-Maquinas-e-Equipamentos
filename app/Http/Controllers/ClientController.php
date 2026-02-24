39189555f3be78b0524b374b8b54b4c94af7fb01
Lucas-JS-Conect <lucasaprendiz1993@gmail.com>
2026-02-24 15:06:15 -0300

    Add Rule validation import in ClientController

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -6,6 +6,7 @@ use App\Models\Client;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class ClientController extends Controller
 {
